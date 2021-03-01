973f80e8ca8ad970183fb7486566f03aeb23d1f1
drdr xp <drdr.xp@gmail.com>
2021-03-01 22:56:38 +0800

    first shot! support Add() only

---
+++ skip.go
@@ -0,0 +1,163 @@
+package skiplist
+
+import (
+	"math/rand"
+	"strings"
+)
+
+var (
+	// the chance to add next level.
+	// a classic impl is 1/2.
+	// usually 1/4 perform best
+	ratio = float64(0.5)
+)
+
+type Elt struct {
+	pointers []*Elt
+
+	keyLen int
+	// there are 3 seg in  kv:
+	// the first byte is elt type: head: 00, tail: 02, other elt: 01
+	// so that head always less than other node and other elts are always less
+	// than tail.
+	kv []byte
+}
+
+type Skiplist struct {
+	head, tail Elt
+}
+
+func New() *Skiplist {
+	s := &Skiplist{}
+	s.head.pointers = make([]*Elt, 1)
+	s.head.pointers[0] = &s.tail
+	s.head.kv = []byte{0}
+	s.tail.kv = []byte{2}
+
+	return s
+}
+
+func NewElt(lvl int, k, v string) *Elt {
+
+	e := &Elt{
+		pointers: make([]*Elt, lvl+1),
+		keyLen:   len(k),
+		// \x01 indicates it is a payload elt.
+		kv: []byte("\x01" + k + v),
+	}
+
+	return e
+}
+
+func (e *Elt) Key() string {
+	return string(e.kv[1 : 1+e.keyLen])
+}
+
+func (e *Elt) cmpKey() string {
+	return string(e.kv[:1+e.keyLen])
+}
+
+func (e *Elt) Value() string {
+	return string(e.kv[1+e.keyLen:])
+}
+
+// Less return true if e < o.
+// e.Less(o) implies !o.Less(e)
+// !e.Less(o) && ! o.Less(e) implies e == o
+func (e *Elt) Less(o *Elt) bool {
+	ka := e.cmpKey()
+	kb := o.cmpKey()
+	return ka < kb
+}
+
+func (e *Elt) String() string {
+	typs := map[byte]string{
+		0: "H:", // head
+		1: "E:", // normal elt
+		2: "T:", // tail
+	}
+	typ := e.kv[0]
+	return typs[typ] + e.Key() + "=" + e.Value()
+}
+
+func (s *Skiplist) String() string {
+	// TODO test
+
+	res := []string{}
+	cur := &s.head
+	for ; cur != &s.tail; cur = cur.pointers[0] {
+		res = append(res, cur.String())
+
+	}
+	res = append(res, s.tail.String())
+	return strings.Join(res, " > ")
+}
+
+// Add a new kv-pair into skiplist, returns true if it overrides an existent
+// record.
+func (s *Skiplist) Add(k, v string) bool {
+	lvl := randLvl(ratio)
+
+	e := NewElt(lvl, k, v)
+	for len(e.pointers) > len(s.head.pointers) {
+		s.head.pointers = append(s.head.pointers, &s.tail)
+	}
+
+	ps, equal := s.searchElt(e)
+
+	if equal {
+		// remove dup elt
+		p := ps[0].pointers[0]
+		for i := 0; i < len(ps); i++ {
+			if ps[i].pointers[i] == p {
+				ps[i].pointers[i] = p.pointers[i]
+			}
+		}
+	}
+
+	for i := 0; i < len(e.pointers); i++ {
+		e.pointers[i] = ps[i].pointers[i]
+		ps[i].pointers[i] = e
+	}
+
+	return equal
+}
+
+// Search for k and returns a slice of *Elt
+// and if the previous elt exactly equal e.
+//
+//   3   ------------------------>
+//   2   -------------------> 2 ->
+//   1   ---------> 1 ------> 1 ->
+//   0 head -> a -> b -> c -> d -> tail
+//
+func (s *Skiplist) searchElt(e *Elt) ([]*Elt, bool) {
+
+	h := &s.head
+
+	lvl := len(h.pointers) - 1
+	rst := make([]*Elt, lvl+1)
+
+	cur := h
+	for ; lvl >= 0; lvl-- {
+
+		// find the first cur that e <= cur.next
+		for cur.pointers[lvl].Less(e) {
+			cur = cur.pointers[lvl]
+		}
+
+		rst[lvl] = cur
+	}
+
+	nxt := cur.pointers[0]
+
+	return rst, !nxt.Less(e) && !e.Less(nxt)
+}
+
+func randLvl(ratio float64) int {
+	x := 0
+	for rand.Float64() < ratio {
+		x++
+	}
+	return x
+}
---
+++ skip_test.go
@@ -0,0 +1,112 @@
+package skiplist
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+
+	ta := require.New(t)
+
+	s := New()
+	ta.Equal(1, len(s.head.pointers))
+	ta.Equal(0, len(s.tail.pointers))
+	ta.Equal(&s.tail, s.head.pointers[0])
+	ta.Equal("\x00", string(s.head.kv))
+	ta.Equal("\x02", string(s.tail.kv))
+}
+
+func Test_randLvl(t *testing.T) {
+
+	ta := require.New(t)
+	ratio := 0.5
+
+	n := 1000 * 1000
+	sample := make([]int, 64)
+	for i := 0; i < n; i++ {
+		v := randLvl(ratio)
+		sample[v]++
+	}
+
+	fmt.Println(sample[:4])
+	for i := 0; i < 4; i++ {
+		got := float64(sample[i+1]) / float64(sample[i])
+		ta.InDelta(ratio, got, float64(ratio)/5, "%d %d", sample[i], sample[i+1])
+	}
+}
+
+func TestNewElt_and_Key_Value(t *testing.T) {
+	ta := require.New(t)
+
+	e := NewElt(3, "foo", "bar")
+	ta.Equal([]*Elt{nil, nil, nil, nil}, e.pointers)
+	ta.Equal("\x01foobar", string(e.kv))
+	ta.Equal(3, e.keyLen)
+
+	ta.Equal("foo", e.Key())
+	ta.Equal("bar", e.Value())
+}
+
+func TestElt_Less(t *testing.T) {
+	ta := require.New(t)
+
+	a := NewElt(5, "aa", "bb")
+	a2 := NewElt(3, "aa", "cc")
+	b := NewElt(3, "ab", "cc")
+
+	ta.False(a.Less(a))
+
+	ta.True(a.Less(b))
+	ta.False(a.Less(a2))
+	ta.False(a2.Less(a))
+	ta.False(b.Less(a))
+
+	s := New()
+	ta.True(s.head.Less(a))
+	ta.True(a.Less(&s.tail))
+}
+
+func TestElt_String(t *testing.T) {
+	ta := require.New(t)
+	a := NewElt(5, "aa", "bb")
+	ta.Equal("E:aa=bb", a.String())
+
+	s := New()
+	ta.Equal("H:=", s.head.String())
+	ta.Equal("T:=", s.tail.String())
+}
+
+func TestSkiplist_String(t *testing.T) {
+
+	ta := require.New(t)
+
+	s := New()
+
+	a := NewElt(5, "aa", "bb")
+	s.head.pointers[0] = a
+	a.pointers[0] = &s.tail
+
+	got := s.String()
+	ta.Equal("H:= > E:aa=bb > T:=", got)
+}
+
+func TestSkiplist_Add_searchElt(t *testing.T) {
+
+	ta := require.New(t)
+
+	s := New()
+	s.Add("a", "")
+	ta.Equal("H:= > E:a= > T:=", s.String())
+
+	s.Add("b", "")
+	ta.Equal("H:= > E:a= > E:b= > T:=", s.String())
+
+	s.Add("c", "")
+	ta.Equal("H:= > E:a= > E:b= > E:c= > T:=", s.String())
+
+	s.Add("b", "newB")
+	ta.Equal("H:= > E:a= > E:b=newB > E:c= > T:=", s.String())
+}
